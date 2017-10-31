837f30f45ceadf8625bd9e452493bbf65f2780c5
赵仲印 <zhaozhongyin@kezaihui.com>
2017-10-31 18:53:21 +0800

    donot compile sass after /*!#-wx*/

--- src/compile-style.js
+++ src/compile-style.js
@@ -18,77 +18,46 @@ module.exports = {
         let dist = cache.getDist();
         let ext = cache.getExt();
 
-        if (typeof styles === 'string') {
-            // .compile('less', opath) 这种形式
-            opath = requires;
-            requires = [];
-            moduleId = '';
-            styles = [{
-                type: styles,
-                scoped: false,
-                code: util.readFile(path.join(opath.dir, opath.base)) || ''
-            }];
-        }
-        let allPromises = [];
+        opath = requires;
+        requires = [];
+        moduleId = '';
+        style = {
+            type: styles,
+            scoped: false,
+            code: util.readFile(path.join(opath.dir, opath.base)) || ''
+        };
+        
 
         // styles can be an empty array
-        styles.forEach((style) => {
-            let lang = style.type || 'css';
-            const content = style.code;
-            const scoped = style.scoped;
+        let lang = style.type || 'css';
+        const content = style.code;
+        const scoped = style.scoped;
 
-            if (lang === 'scss')
-                lang = 'sass';
+        if (lang === 'scss')
+            lang = 'sass';
 
-            let compiler = loader.loadCompiler(lang);
+        let compiler = loader.loadCompiler(lang);
 
-            if (!compiler) {
-                throw `未发现相关 ${lang} 编译器配置，请检查wepy.config.js文件。`;
-            }
-
-            const p = compiler(content, config.compilers[lang] || {}, path.join(opath.dir, opath.base)).then((css) => {
-                // 处理 scoped
-                if (scoped) {
-                    // 存在有 scoped 的 style
-                    return scopedHandler(moduleId, css).then((cssContent) => {
-                        return cssContent;
-                    });
-                } else {
-                    return css;
-                }
-            });
-
-            allPromises.push(p);
-        });
-        Promise.all(allPromises).then((rets) => {
+        if (!compiler) {
+            throw `未发现相关 ${lang} 编译器配置，请检查wepy.config.js文件。`;
+        }
+        let needCompile = content;
+        let doNotCompile = '';
+        // 对于/\*!#-wx\*/注释后的内容不进行compile，
+        const pos = content.indexOf('/*!#-wx*/');
+        if (pos!== -1) {
+            needCompile = content.substr(0,pos);
+            doNotCompile = content.substr(pos);
+        }
+        const p = compiler(
+            needCompile, config.compilers[lang] || {}, path.join(opath.dir, opath.base)
+        );
+        Promise.all([p]).then(rets => {
             let allContent = rets.join('');
-            if (requires && requires.length) {
-                requires.forEach((r) => {
-                    let comsrc = util.findComponent(r);
-                    let isNPM = false;
-
-                    if (!comsrc) {
-                        util.log('找不到组件：' + r, '错误');
-                    } else {
-                        if (comsrc.indexOf('node_modules') > -1) {
-                            comsrc = util.getDistPath(path.parse(comsrc));
-                            comsrc = comsrc.replace(ext, '.wxss').replace(`${path.sep}${dist}${path.sep}`, `${path.sep}${src}${path.sep}`);
-                            isNPM = true;
-                        }
-                        let relative = path.relative(opath.dir + path.sep + opath.base, comsrc);
-                        let code = util.readFile(comsrc);
-                        if (isNPM || /<style/.test(code)) {
-                            relative = relative.replace(ext, '.wxss').replace(/\\/ig, '/').replace('../', './');
-                            allContent = '@import "' + relative + '";\n' + allContent;
-                        }
-                    }
-                });
-            }
             let target = util.getDistPath(opath, 'wxss', src, dist);
-
             let plg = new loader.PluginHelper(config.plugins, {
                 type: 'css',
-                code: allContent,
+                code: allContent + doNotCompile.replace(/.scss/g, '.wxss'),
                 file: target,
                 output (p) {
                     util.output(p.action, p.file);
@@ -100,6 +69,6 @@ module.exports = {
             });
         }).catch((e) => {
             console.log(e);
-        });
+        })
     }
 }
